f7696c6692ab9cc4eeedb49c8c56a91371b9ab6d
Polar_Summer <tenesaty@yahoo.de>
2021-01-10 12:54:12 +0100

    [API] First functional request to our data possible

--- Hua-Mulan/api.py
+++ Hua-Mulan/api.py
@@ -1,15 +1,26 @@
 import flask
 from flask import request, jsonify
+from indexing.index_connector import IndexConnector
+from retrieval.ranker import rank
+
+dataConn = IndexConnector("localhost", "9200", "args")
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/api/v1/resources/books', methods=['GET'])
+@app.route('/', methods=['GET'])
 def home():
-    if 'id' in request.args:
-        id = int(request.args['id'])
+    return "Welcome to the API of Hua-Mulan"
+
+
+@app.route('/api/query', methods=['GET'])
+def api_arg():
+    if 'arg' in request.args:
+        result = dataConn.query_index(request.args['arg'], 10)
+        return jsonify(result["hits"]["hits"])
     else:
-        return "Error: No id field provided. Please specify an id."
+        return "Error, invalid request"
+
 
 app.run()
