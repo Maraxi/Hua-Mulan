c3bd9ff7c8f7d70a317baa8fc320a527fc88acac
Polar_Summer <tenesaty@yahoo.de>
2021-02-09 01:50:05 +0100

    Added comments for nDCG

--- Hua-Mulan/evaluation/ndcg.py
+++ Hua-Mulan/evaluation/ndcg.py
@@ -3,7 +3,10 @@ import requests
 import json
 import math
 
+# Evaluate our setup with nDCG marker
 class Evaluator:
+
+    # Init with reference data
     def __init__(self):
         dir = __file__[:__file__.index('Hua-Mulan')]
         with open(f'{dir}/Hua-Mulan/data/touche2020-task1-relevance-args-me-corpus-version-2020-04-01.qrels') as reference:
@@ -11,19 +14,23 @@ class Evaluator:
             data = data.splitlines()
             self.data = [line.split() for line in data]
 
+    # Find for respective topic within a document, which score does it have in our reference data
     def reference_score(self, topic, docid):
         score = [float(x[3]) for x in self.data if x[0]==topic and x[2]==docid]
-        if len(score)>0:
+        if len(score) > 0:
             return score[0]
         else:
             return -2.0
 
+    # Get scores for respective topic
     def get_scores(self, query_results):
         return [self.reference_score(doc['nr'], doc['arg_id']) for doc in query_results]
 
+    # DCG on respective topic
     def dcg(self, scores):
         return sum(map(lambda x: x[1]/math.log(x[0]+2,2), enumerate(scores)))
 
+    # IDCG (optimum) on respective topic
     def idcg(self, topic, n):
         relevant = [float(doc[3]) for doc in self.data if doc[0] == topic]
         relevant.sort()
@@ -31,12 +38,15 @@ class Evaluator:
         relevant += [-2]*10
         return self.dcg(relevant[:n])
 
+    # nDCG on respective topic
     def ndcg(self, query):
         scores = self.get_scores(query)
         return self.dcg(scores)/self.idcg(query[0]['nr'], len(scores))
 
 
 dir = __file__[:__file__.index('Hua-Mulan')]
+
+# This part is analogue to evaluate.py
 tree = ET.parse(f'{dir}/Hua-Mulan/Hua-Mulan/evaluation/topics.xml')
 root = tree.getroot()
 
