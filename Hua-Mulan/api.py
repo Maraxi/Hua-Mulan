b6376e71449f10091d4c92d31924cac22fe241ff
mailach <mailach@mailbox.org>
2021-02-10 23:35:18 +0100

    [infrastructure] adapted api and docker files to work together

--- Hua-Mulan/api.py
+++ Hua-Mulan/api.py
@@ -1,13 +1,18 @@
 import flask
 from flask import request, jsonify
 from indexing.index_connector import IndexConnector
+import sys
+import time
 from retrieval.ranker import rank
 
-dataConn = IndexConnector("args")
+
+conn = IndexConnector()
 
 app = flask.Flask(__name__)
+
 app.config["DEBUG"] = True
 
+print("Searchengine can be contacted on Port 5000")
 
 @app.route('/', methods=['GET'])
 def home():
@@ -16,11 +21,13 @@ def home():
 
 @app.route('/api/query', methods=['GET'])
 def api_arg():
-    if 'arg' in request.args:
-        result = dataConn.query_index(request.args['arg'], 10)
+
+    if 'arg' in request.args and 'index' in request.args:
+        print(request.args["index"], flush=True)
+        result = conn.query_index(request.args["arg"], 10, request.args["index"])
         return jsonify(result["hits"]["hits"])
     else:
         return "Error, invalid request"
 
 
-app.run()
+app.run(port=5000, debug=True, host='0.0.0.0')
--- Hua-Mulan/index.py
+++ Hua-Mulan/index.py
@@ -1,9 +1,8 @@
 from indexing.index_connector import IndexConnector
 import pandas as pd
 import time
+
 time.sleep(30)
-import requests
-from urllib.request import urlopen
 
 index = ["args_t5expansion", "args_baseline"]
 file = ["exploration/data_expanded_t5.csv", "exploration/data_baseline.csv"]
@@ -13,7 +12,7 @@ conn = IndexConnector()
 # index = "args_gpt2expansion"
 # index = "args_naiveexpansion"
 i = 1
-while i==0:
+while i == 0:
     for i in range(1):
 
         if not conn.exists(index[i]):
@@ -23,10 +22,4 @@ while i==0:
             data = pd.read_csv(file[i], sep="\t")
             conn.index_bulk(data, index)
 
-
-
-result = conn.query_index("Is Sexual Orientation Determined at Birth?", 10, index[0])
-
-for hit in result["hits"]["hits"]:
-    print(hit["_id"])
-    print(hit["_source"]["conclusion"])
+print("Finish indexing...")
