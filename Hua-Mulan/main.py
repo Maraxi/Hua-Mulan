a9dcd63e57d8108ac3d7e96b530172bbd5918071
mailach <mailach@mailbox.org>
2021-02-12 12:39:14 +0100

    clean up of repo, rm unnessecary files

--- Hua-Mulan/main.py
+++
@@ -1,8 +0,0 @@
-from indexing.index_connector import IndexConnector
-from retrieval.ranker import rank
-
-conn = IndexConnector("args")
-
-result = conn.query_index("nuclear power or solar energy", 100)
-for arg in rank(result["hits"]["hits"]):
-    print(arg)
--- Hua-Mulan/retrieval/ranker.py
+++
@@ -1,4 +0,0 @@
-
-def rank(arguments):
-    return arguments
-
