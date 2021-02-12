ccba11d0e452aa34575eeab705e0fc46b87587bf
mailach <mailach@mailbox.org>
2021-02-12 02:18:47 +0100

    [indexing] Fixed bug in index.py which prevented indexing

--- Hua-Mulan/indexing/index.py
+++ Hua-Mulan/indexing/index.py
@@ -1,11 +1,12 @@
 from index_connector import IndexConnector
 import pandas as pd
 import time
+from os import walk
 
 time.sleep(30)
 
 index = ["args_t5expansion", "args_baseline", "args_naiveexpansion"]
-file = ["data/data_expanded_t5_3queries.csv", "data/data_baseline.csv", "data/data_naiveexpansion.csv"]
+file = ["/pred/data/data_expanded_t5_3queries.csv", "data/data_baseline.csv", "data/data_naiveexpansion.csv"]
 
 conn = IndexConnector()
 
@@ -15,6 +16,6 @@ if not conn.exists(index[0]):
 
 if conn.count(index[0]) == 0:
     data = pd.read_csv(file[0], sep="\t")
-    conn.index_bulk(data, index)
+    conn.index_bulk(data, index[0])
 
 print("Finish indexing...")
